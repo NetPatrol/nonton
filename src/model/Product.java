e19332296eb7bcb80f30eaec8ea1826277fd6139
root <madraddog@gmail.com>
2023-06-07 05:23:21 +0300

    Initial commit

---
+++ src/model/Product.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Poduct {
+    private String id;
+    private String name;
+
+    public Poduct(final String id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
